ff63b3420b8f61d9ef80e3a6722f851977f3af8a
bruno <bschmittcornet@gmail.com>
2023-10-02 16:33:45 +0200

    fix tags.php

--- tags.php
+++ tags.php
@@ -52,7 +52,6 @@
                 $lesInformations = $mysqli->query($laQuestionEnSql);
                 $tag = $lesInformations->fetch_assoc();
                 //@todo: afficher le résultat de la ligne ci dessous, remplacer XXX par le label et effacer la ligne ci-dessous
-                echo "<pre>" . print_r($tag, 1) . "</pre>";
                 ?>
                 <img src="user.jpg" alt="Portrait de l'utilisatrice"/>
                 <section>
