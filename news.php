3fba106a4871c53fc78e45dd652f08ba9461562a
Aisseta <diaw.aisseta@gmail.com>
2023-10-03 10:30:19 +0200

    refactoring config.php

--- news.php
+++ news.php
@@ -49,7 +49,7 @@
                  */
 
                 // Etape 1: Ouvrir une connexion avec la base de donnée.
-                $mysqli = new mysqli("localhost", "root", "root", "socialnetwork");
+                include 'config.php';
                 //verification
                 if ($mysqli->connect_errno)
                 {
